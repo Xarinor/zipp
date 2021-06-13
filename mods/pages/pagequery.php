b220cb84f990621a8deef4852d6f21be8f03447e
Xarinor <lukas.walliser@gmail.com>
2021-06-14 00:45:31 +0200

    First draft getter pagesquery->orderByCTN - works almost as intended - known bug: page-images not accessible in template

--- mods/pages/pagequery.php
+++ mods/pages/pagequery.php
@@ -6,7 +6,9 @@
 
 namespace Pages;
 
+use DateTime;
 use Themes\Theme;
+use function MagmaTemplate\build_ctnSorter;
 
 class PageQuery {
 
@@ -55,6 +57,26 @@ class PageQuery {
 	public function orderAsc( string $key ) {
 		$this->order = [true, $key];
 		return $this;
+	}
+
+	public function orderByCTN(string $field, string $order = null) {
+
+		$pages = $this->go();
+
+		foreach ( $pages as $p ) {
+			$ly = $this->theme->resolvePage( $p );
+			$p->replaceCtn( $ly->fillFields( $p ) );
+		}
+
+		if ($order == 'random') {
+			shuffle($pages);
+		} else if ($order == null || $order == 'ascending') {
+			usort($pages, build_CTN_Sorter($field, true));
+		} else if ($order == 'descending') {
+			usort($pages, build_CTN_Sorter($field, false));
+		}
+
+		return $this->filterAmount( $pages );
 	}
 
 	public function limit( int $amount ) {
@@ -120,4 +142,36 @@ class PageQuery {
 
 	// query
 
+}
+
+function build_CTN_Sorter($key, $flip) {
+	return function ($inA, $inB) use ($key, $flip) {
+
+		if ($inA->$key instanceof DateTime) {
+			$outA = $inA->$key->format("U");
+		} else if ($inA == '') {
+			$outA = -1;
+		} else if (is_string($inA->$key)) {
+			$outA = $inA->$key;
+		} else if (!is_string($inA->$key) && is_string(strval($inA->$key))) {
+			$outA = strval($inA->$key);
+		} else {
+			$outA = 0;
+		}
+		if ($inB->$key instanceof DateTime) {
+			$outB = $inB->$key->format("U");
+		} else if ($inB == '') {
+			$outB = -1;
+		} else if (is_string($inB->$key)) {
+			$outB = $inB->$key;
+		} else if (!is_string($inB->$key) && is_string(strval($inB->$key))) {
+			$outB = strval($inB->$key);
+		} else {
+			$outB = 0;
+		}
+
+		$result = strnatcmp($outA, $outB);
+		$result = $flip ? ($result * -1) : $result;
+		return $result;
+	};
 }
